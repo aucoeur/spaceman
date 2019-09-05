03934276ba7f53b76a9ea4b96a347cc933699ed8
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-09-05 14:42:21 -0700

    Test set & intersection

--- str_test.py
+++ str_test.py
@@ -1,15 +1,31 @@
 test = ['s','g','d','t','y','g','l','w','b','r']
 secret_word = 'stare'
+
 reveal = [x for x in secret_word if x in test]
 hide = [x for x in secret_word if x not in test]
+
+underscore = '_'
+ans_list = []
+
 print(secret_word)
 print(len(secret_word))
 
 def check():
-    print(reveal)
-    print(hide[0].replace(hide[0], '_'))
-
-
+    pass
+    
 def check2():
-    print(set(secret_word).intersection(test))
-    print(set(secret_word).difference(test))
+    print(frozenset(secret_word).intersection(test))
+    print(frozenset(secret_word).difference(test))
+
+
+print(reveal)
+print(hide)
+
+def test1():
+    for item in secret_word:
+        if item in test:
+            ans_list.append(item)
+        else:
+            ans_list.append(underscore)
+
+    print(' '.join(ans_list))
